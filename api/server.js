05f8d5a38a9a8115b3fabd07b62c0336ac5b1a93
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-10-21 00:16:19 -0400

    UPDATE/PATCH models and endpoints created for zips and zones. Added a findById and similar for zones as well. This allows the zipcode or zone in zones to be changed without error

--- api/server.js
+++ api/server.js
@@ -17,29 +17,23 @@ server.get("/", (req, res) => {
   res.send(
     `<div>
       <h1>Welcome to Sevens Seas Zone and Zip Database API!</h1>
-      <h3>Data displayed will be raw json formatting</h3>
+      <h2>Data displayed will be RAW JSON formatting</h2>
+      <h3>To view source code for this DB: https://github.com/RSibaja88/SQL-SSK</h3>
       <p>
         To fetch information from the API, add the following to the end of the
         URL:
       </p>
-      <p>"/api/everything" - This will show you all of the data</p>
       <p>"/api/zips" - This will show you all of the zipcodes included in the DB</p>
       <p>"/api/zips/:zipcode" - (IE: /api/zips/92106) This will show you the data attached to a specific zipcode</p>
       <p>"/api/zips/zone/:zone" - (IE: /api/zips/zone/east)This will show you all of the zipcodes in that particular zone</p>
       <p>"/api/:zone" - (IE: /api/east)This will show you the schedule for that particular zone. Use all lowercase for zone in URL</p>
+      <ul><p>Other API functions created:</p>
+      <li>Update Existing Zipcode Entry</li>
+      <li>Update Existing Zone Schedule</li>
+      <li>Add new Zipcode entry</li>
+      <li>Delete a specific Zipcode Entry</li> </ul>
     </div>`
   );
-});
-
-server.get("/api/everything", (req, res) => {
-  dModel
-    .findEverything()
-    .then((items) => {
-      res.status(200).json(items);
-    })
-    .catch((error) => {
-      res.status(500).json({ message: "Couldn't fetch API.", error });
-    });
 });
 
 server.get("/api/zips", (req, res) => {
@@ -57,6 +51,25 @@ server.get("/api/zips/:zipcode", (req, res) => {
   const { zipcode } = req.params;
   dModel
     .findByZip(zipcode)
+    .then((zip) => {
+      if (zip) {
+        res.status(200).json(zip);
+      } else {
+        res.status(404).json({ message: "Zipcode not found in database" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Couldn't find that specific Zipcode's information",
+        error,
+      });
+    });
+});
+
+server.get("/api/zips/id/:id", (req, res) => {
+  const { id } = req.params;
+  dModel
+    .findByID(id)
     .then((zip) => {
       if (zip) {
         res.status(200).json(zip);
@@ -128,6 +141,90 @@ server.get("/api/south", (req, res) => {
     .catch((error) => {
       res.status(500).json({
         message: "Couldn't fetch the schedule for the South side.",
+        error,
+      });
+    });
+});
+
+//UPDATE EXISTING ZIPCODE DATA
+server.patch("/api/zips/editing/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  dModel
+    .updateZip(id, changes)
+    .then((newZip) => {
+      if (newZip) {
+        res.status(200).json(newZip);
+      } else {
+        res.status(404).json({ message: "Zipcode not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "ERROR editing your entry",
+        error,
+      });
+    });
+});
+
+//UPDATE EXISTING EAST SCHEDULE
+server.patch("/api/east/editing/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  dModel
+    .updateEast(id, changes)
+    .then((newZone) => {
+      if (newZone) {
+        res.status(200).json(newZone);
+      } else {
+        res.status(404).json({ message: "Zone Schedule not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "ERROR editing your entry",
+        error,
+      });
+    });
+});
+
+//UPDATE EXISTING WEST SCHEDULE
+server.patch("/api/west/editing/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  dModel
+    .updateWest(id, changes)
+    .then((newZone) => {
+      if (newZone) {
+        res.status(200).json(newZone);
+      } else {
+        res.status(404).json({ message: "Zone Schedule not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "ERROR editing your entry",
+        error,
+      });
+    });
+});
+
+//UPDATE EXISTING SOUTH SCHEDULE
+server.patch("/api/south/editing/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  dModel
+    .updateSouth(id, changes)
+    .then((newZone) => {
+      if (newZone) {
+        res.status(200).json(newZone);
+      } else {
+        res.status(404).json({ message: "Zone Schedule not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "ERROR editing your entry",
         error,
       });
     });
--- sskData/SSKDataModel.js
+++ sskData/SSKDataModel.js
@@ -5,15 +5,10 @@ const knex = require("knex");
 const config = require("../knexfile");
 const db = knex(config.development);
 
-//ADD ZIP
+//POST ZIP
 async function addZip(zip) {
   const [id] = await db("zips").insert(zip);
   return id;
-}
-
-//GETS EVERYTHING
-function findEverything() {
-  return db(["west", "east", "south", "zips"]);
 }
 
 //GET SPECIFIC ZONES DATA
@@ -40,8 +35,65 @@ function findByZip(zipcode) {
   return db("zips").where({ zipcode }).first();
 }
 
+function findByID(id) {
+  return db("zips").where({ id }).first();
+}
+
 function findByZone(zone) {
   return db("zips").where({ zone });
+}
+
+//GET ZONE BY ID
+function eastByID(id) {
+  return db("east").where({ id }).first();
+}
+
+function westByID(id) {
+  return db("west").where({ id }).first();
+}
+
+function southByID(id) {
+  return db("south").where({ id }).first();
+}
+
+//UPDATE Existing Zipcode
+async function updateZip(id, changes) {
+  return await db("zips")
+    .where({ id })
+    .update(changes)
+    .then(() => {
+      return findByID(id);
+    });
+}
+
+//UPDATE Existing East Schedule
+async function updateEast(id, changes) {
+  return await db("east")
+    .where({ id })
+    .update(changes)
+    .then(() => {
+      return eastByID(id);
+    });
+}
+
+//UPDATE Existing West Schedule
+async function updateWest(id, changes) {
+  return await db("west")
+    .where({ id })
+    .update(changes)
+    .then(() => {
+      return westByID(id);
+    });
+}
+
+//UPDATE Existing South Schedule
+async function updateSouth(id, changes) {
+  return await db("south")
+    .where({ id })
+    .update(changes)
+    .then(() => {
+      return southByID(id);
+    });
 }
 
 //DELETE ZIP
@@ -57,6 +109,13 @@ module.exports = {
   findZips,
   findByZip,
   findByZone,
-  findEverything,
+  findByID,
+  eastByID,
+  westByID,
+  southByID,
   removeZip,
+  updateZip,
+  updateEast,
+  updateWest,
+  updateSouth,
 };
